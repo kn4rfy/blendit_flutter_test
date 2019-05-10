8078f305c67e273b27966ac7e4a5fbda9b98bb8f
Francis Knarfy Elopre <kn4rfy@gmail.com>
2019-05-10 17:48:18 +0800

    Move plotting of routes to map.dart

--- lib/directions.dart
+++ lib/directions.dart
@@ -1,12 +1,11 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:async/async.dart';
+
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 
-import 'page.dart';
 import 'map.dart';
+import 'page.dart';
 
 class Directions extends Page {
 	Directions() : super(const Icon(Icons.map), 'Directions');
@@ -39,10 +38,6 @@ class DirectionsBodyState extends State<DirectionsBody> {
 	FocusNode _destinationFocus = FocusNode();
 	String _destination = '';
 	bool _isDestinationFocused = false;
-
-	LatLngBounds _bounds;
-	CameraPosition _cameraPosition;
-	Map<PolylineId, Polyline> _routes = <PolylineId, Polyline>{};
 
 	@override
 	void initState() {
@@ -149,45 +144,6 @@ class DirectionsBodyState extends State<DirectionsBody> {
 		});
 	}
 
-/*	void getCoordinatesByGeocode(value, fieldName) async {
-		final response =
-		await http.get(
-			'https://maps.googleapis.com/maps/api/geocode/json?address=$value&key=$_googleMapsApiKey');
-
-		if (response.statusCode == 200) {
-			setCoordinates(response.body, fieldName);
-		} else {
-			if (fieldName == '_originController') {
-				setState(() {
-					_origin = value;
-				});
-			} else {
-				setState(() {
-					_destination = value;
-				});
-			}
-		}
-	}
-
-	void setCoordinates(data, fieldName) {
-		debugPrint('getCoordinatesByGeocode: $data');
-
-		var coordinates = json.decode(data);
-
-		var lat = coordinates['results'][0]['geometry']['location']['lat'];
-		var lng = coordinates['results'][0]['geometry']['location']['lng'];
-
-		if (fieldName == '_originController') {
-			setState(() {
-				_origin = '$lat,$lng';
-			});
-		} else {
-			setState(() {
-				_destination = '$lat,$lng';
-			});
-		}
-	}*/
-
 	void getDirections() async {
 		setState(() {
 			_isLoading = true;
@@ -215,98 +171,6 @@ class DirectionsBodyState extends State<DirectionsBody> {
 			ErrorWidget('Error! Please try again.');
 		}
 	}
-
-//	void processDirectionsData(data) {
-//		var directions = json.decode(data);
-//
-//		setState(() {
-//			_bounds = LatLngBounds(
-//				northeast: LatLng(
-//					directions['routes'][0]['bounds']['northeast']['lat'],
-//					directions['routes'][0]['bounds']['northeast']['lng']),
-//				southwest: LatLng(
-//					directions['routes'][0]['bounds']['southwest']['lat'],
-//					directions['routes'][0]['bounds']['southwest']['lng']),
-//			);
-//
-//			_cameraPosition = CameraPosition(
-//				target: LatLng(
-//					directions['routes'][0]['legs'][0]['start_location']['lat'],
-//					directions['routes'][0]['legs'][0]['start_location']['lng']),
-//				zoom: 12.0,
-//			);
-//		});
-//
-//		List<dynamic> steps = directions['routes'][0]['legs'][0]['steps'];
-//		String points = '';
-//
-//		int counter = 0;
-//
-//		steps.forEach((step) {
-//			var startLat = step['start_location']['lat'];
-//			var startLng = step['start_location']['lng'];
-//			var endLat = step['end_location']['lat'];
-//			var endLng = step['end_location']['lng'];
-//
-//			if(counter == 0){
-//				points += '$startLat,$startLng|$endLat,$endLng';
-//			} else {
-//				points += '|$endLat,$endLng';
-//			}
-//			counter ++;
-//		});
-//
-//		final PolylineId polylineId = PolylineId(
-//			directions['routes'][0]['summary']);
-//
-//		getSnappedRoutes(points, polylineId);
-//	}
-//
-//	void getSnappedRoutes(path, id) async {
-//		final response =
-//		await http.get(
-//			'https://roads.googleapis.com/v1/snapToRoads?path=$path&interpolate=true&key=$_googleMapsApiKey');
-//
-//		if (response.statusCode == 200) {
-//			setPolylines(response.body, id);
-//		} else {
-////			_routes[id] = Polyline(
-////				polylineId: id,
-////				points: path,
-////				color: Colors.blue,
-////			);
-//		}
-//	}
-//
-//	void setPolylines(data, id) {
-//		debugPrint('setPolylines $data');
-//
-//		var snappedRoutes = json.decode(data);
-//
-//		List<dynamic> snappedPoints = snappedRoutes['snappedPoints'];
-//		List<LatLng> points = <LatLng>[];
-//
-//		snappedPoints.forEach((snappedPoint) {
-//			points.add(LatLng(
-//				snappedPoint['location']['latitude'], snappedPoint['location']['longitude']));
-//		});
-//
-//		setState(() {
-//			_routes[id] = Polyline(
-//				polylineId: id,
-//				points: points,
-//				color: Colors.blue,
-//			);
-//			_isLoading = false;
-//		});
-//
-//		Navigator.push(
-//			context,
-//			MaterialPageRoute(
-//				builder: (context) => MapView(_bounds, _cameraPosition, _routes)
-//			),
-//		);
-//	}
 
 	@override
 	Widget build(BuildContext context) {
--- lib/map.dart
+++ lib/map.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:http/http.dart' as http;
 
 class MapView extends StatefulWidget {
 	final data;
