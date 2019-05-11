ee153148c912fbe219748ae18b86212234ad2b5f
Francis Knarfy Elopre <kn4rfy@gmail.com>
2019-05-11 13:31:38 +0800

    Remove example guides

--- lib/main.dart
+++ lib/main.dart
@@ -4,56 +4,9 @@
 
 import 'package:flutter/material.dart';
 
-import 'animate_camera.dart';
 import 'directions.dart';
-import 'map_coordinates.dart';
-import 'map_ui.dart';
-import 'marker_icons.dart';
-import 'move_camera.dart';
-import 'page.dart';
-import 'place_marker.dart';
-import 'place_polyline.dart';
-import 'scrolling_map.dart';
 
-final List<Page> _allPages = <Page>[
-	Directions(),
-	MapUiPage(),
-	MapCoordinatesPage(),
-	AnimateCameraPage(),
-	MoveCameraPage(),
-	PlaceMarkerPage(),
-	MarkerIconsPage(),
-	ScrollingMapPage(),
-	PlacePolylinePage(),
-];
-
-class MyApp extends StatelessWidget {
-	void _pushPage(BuildContext context, Page page) {
-		Navigator.of(context).push(MaterialPageRoute<void>(
-			builder: (_) =>
-				Scaffold(
-					appBar: AppBar(title: Text(page.title)),
-					body: page,
-				)));
-	}
-
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			appBar: AppBar(title: const Text('GoogleMaps examples')),
-			body: ListView.builder(
-				itemCount: _allPages.length,
-				itemBuilder: (_, int index) =>
-					ListTile(
-						leading: _allPages[index].leading,
-						title: Text(_allPages[index].title),
-						onTap: () => _pushPage(context, _allPages[index]),
-					),
-			),
-		);
-	}
-}
 
 void main() {
-	runApp(MaterialApp(home: MyApp()));
+	runApp(MaterialApp(home: Directions()));
 }
