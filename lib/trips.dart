362ad968a3d5ce5a5f0796d5ebacfabbd7e1b0a9
Francis Knarfy Elopre <kn4rfy@gmail.com>
2019-05-19 01:40:32 +0800

    Trips UI

---
+++ lib/trips.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+
+class TripsView extends StatefulWidget {
+	TripsView();
+
+	@override
+	TripsState createState() => TripsState();
+}
+
+class TripsState extends State<TripsView> {
+
+	@override
+	void initState() {
+		super.initState();
+	}
+
+	@override
+	void dispose() {
+		super.dispose();
+	}
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: AppBar(title: Text('Trips')),
+			body: Column(
+				children: <Widget>[
+					Padding(
+						padding: EdgeInsets.fromLTRB(32, 16, 0, 16),
+						child: Container(
+							child: Padding(
+								padding: EdgeInsets.all(32),
+								child: Column(
+									crossAxisAlignment: CrossAxisAlignment
+										.start,
+									mainAxisSize: MainAxisSize.min,
+									children: <Widget>[
+										Text(
+											'Your Trips',
+											style: TextStyle(
+												fontSize: 36,
+												fontWeight: FontWeight.bold,
+												color: Color(0xFF0c63b6),
+											),
+										),
+										Padding(
+											padding: const EdgeInsets.only(
+												top: 16),
+											child: Row(
+												children: <Widget>[
+													Container(
+														child: Padding(
+															padding: EdgeInsets
+																.all(8),
+															child: Icon(
+																Icons
+																	.add_location,
+																size: 20,
+																color: Color(
+																	0xFFFFFFFF),
+															),
+														),
+														decoration: BoxDecoration(
+															color: Color(
+																0xFFfe8a7f),
+															borderRadius: BorderRadius
+																.circular(36)
+														),
+													),
+													Padding(
+														padding: const EdgeInsets
+															.only(left: 16),
+														child: Column(
+															crossAxisAlignment: CrossAxisAlignment
+																.start,
+															children: <Widget>[
+																Text(
+																	'TBLISI',
+																	style: TextStyle(
+																		fontSize: 18,
+																		fontWeight: FontWeight
+																			.bold,
+																		color: Color(
+																			0xFF0c63b6),
+																	),
+																),
+																Padding(
+																	padding: EdgeInsets.all(2),
+																),
+																Text(
+																	'Current location',
+																	style: TextStyle(
+																		fontSize: 15,
+																		color: Color(
+																			0xFF0c63b6),
+																	),
+																),
+															],
+														),
+													)
+												],
+											),
+										)
+									],
+								),
+							),
+							decoration: BoxDecoration(
+								color: Color(0xFFFFFFFF),
+								borderRadius: BorderRadius.horizontal(
+									left: Radius.circular(32))
+							),
+						),
+					),
+					Expanded(
+						child: ListView(),
+					),
+					Container(
+						child: Padding(
+							padding: EdgeInsets.all(32),
+							child: Row(
+							  children: <Widget>[
+								  Expanded(
+							      child: Column(
+							      	crossAxisAlignment: CrossAxisAlignment
+							      		.start,
+							      	mainAxisSize: MainAxisSize.min,
+							      	children: <Widget>[
+							      		Text(
+							      			'Upcoming Trips',
+							      			style: TextStyle(
+							      				fontSize: 36,
+							      				fontWeight: FontWeight.bold,
+							      				color: Color(0xFF0c63b6),
+							      			),
+										    textAlign: TextAlign.left
+							      		),
+							      		Padding(
+										      padding: EdgeInsets.all(16),
+									      ),
+							      		Text(
+							      			'Reservations, Homes, Hotels, Things to Do...',
+							      			style: TextStyle(
+							      				color: Color(0xFF0c63b6),
+							      			),
+										    textAlign: TextAlign.left
+							      		),
+							      ],),
+							    ),
+								  Expanded(
+								    child: Align(
+								      child: FlatButton(
+									  onPressed: () => {},
+									    child: Text(
+										    'See more',
+										    style: TextStyle(
+											    fontWeight: FontWeight.bold,
+											    color: Color(0xFF0c63b6),
+										    ),
+										    textAlign: TextAlign.left
+									    ),
+								      ),
+								    ),
+								  )
+							  ],
+							),
+						),
+					),
+				],
+			),
+		);
+	}
+}
